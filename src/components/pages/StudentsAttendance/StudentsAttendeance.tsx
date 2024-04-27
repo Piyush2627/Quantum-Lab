016ce5bd83f57cf4d51842fa92273a0e9aaa6762
Piyush Dahiwadkar <97776547+Piyush2627@users.noreply.github.com>
2024-04-27 13:15:24 +0530

    naigation update

--- src/components/pages/StudentsAttendance/StudentsAttendeance.tsx
+++ src/components/pages/StudentsAttendance/StudentsAttendeance.tsx
@@ -1,26 +1,43 @@
 import TextRevel from "../../animation/TextRevel/TextRevel";
 import SearchIcon from "@mui/icons-material/Search";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+
 function StudentsAttendeance() {
+  let navigate = useNavigate();
+
   return (
     <div>
-      <TextRevel>
-        <div className=" text-center font-bold text-xl mt-5 ">
+      <div className="flex">
+        <button
+          className="p-1 m-2 border rounded-full hover:bg-violet-200"
+          onClick={() => navigate(-1)}
+        >
+          <ArrowBackIcon fontSize="large" />
+        </button>
+        <div className="w-full text-center font-bold text-xl mt-5 ">
           Student Attendance Management Portal
         </div>
-      </TextRevel>
+      </div>
+
       <TextRevel>
         <div className="p-5 m-5  border rounded-md">
           <div className="text-xl font-semibold">Find your profile</div>
           <div className="mt-3">
-            <div className="bg-violet-400 w-max flex items-center rounded-md">
+            <div className="bg-violet-500 w-max flex items-center rounded-md">
               <SearchIcon className=" text-white pl-2" fontSize="large" />
               <input
                 type="text"
-                className="items-cente flex r pl-2 w-full h-10 bg-violet-400 text-white text-base focus:ring-0 focus:border-0 active:border-0 active:ring-0 placeholder:text-white"
+                className="items-cente flex r pl-2 w-full h-10 bg-violet-500 text-white focus:ring-0 focus:border-0 active:border-0 active:ring-0 placeholder:text-white"
                 placeholder="Your ID"
               />
-              <button className="px-4 text-white ">search</button>
+              <button className="px-4 text-white ">Search</button>
             </div>
+          </div>
+          <div className="mt-5 space-x-5 flex justify-between">
+            <div className="p-5 rounded-md border-2 w-full">Section 1</div>
+            <div className="p-5 rounded-md border-2 w-full">Section 2</div>
+            <div className="p-5 rounded-md border-2 w-full">Section 3</div>
           </div>
         </div>
       </TextRevel>
